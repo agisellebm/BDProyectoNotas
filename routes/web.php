e9626829005ddb4ccf4430bfec97de511ce8a9ee
alexiaruizq <alexiaruiz-@hotmail.com>
2021-03-25 00:22:04 -0700

    hola

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Models\Notas;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\NotasController@welcome' );
+
+Route::get('notas', 'App\Http\Controllers\NotasController@index')->name('notas.index');
+
+Route::get('agregar', 'App\Http\Controllers\NotasController@agregar');
+
+Route::post('crear', 'App\Http\Controllers\NotasController@crear')->name('notas.store');
+
+Route::get('notas/{id}/editar', 'App\Http\Controllers\NotasController@editar')->name('notas.edit');
+
+Route::put('notas/{notas}/editar', 'App\Http\Controllers\NotasController@update')->name('notas.update');
+
+Route:: delete('notas/{id}', 'App\Http\Controllers\NotasController@destroy')->name('notas.destroy');
